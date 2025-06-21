b45685ee50de3900d7c7c3c6744658b74310dffc
Aleksandra <a.strzelczyk.263@studms.ug.edu.pl>
2025-06-21 14:59:55 +0200

    Update index.js

--- kino-frontend/kino-frontend/src/index.js
+++ kino-frontend/kino-frontend/src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-// Tworzymy root i renderujemy App
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -12,5 +11,4 @@ root.render(
   </React.StrictMode>
 );
 
-// Pomiar wydajności (opcjonalny)
 reportWebVitals();
