b25e595894b8b0abc20cf83bd7e54d8488a9c0e1
Aleksandra <a.strzelczyk.263@studms.ug.edu.pl>
2025-06-21 15:05:32 +0200

    Update Screening.cs

--- CinemaReservationApi/CinemaReservationApi/Models/Screening.cs
+++ CinemaReservationApi/CinemaReservationApi/Models/Screening.cs
@@ -4,11 +4,11 @@ public class Screening
 {
     public int Id { get; set; }
 
-    public int MovieId { get; set; } // To jest klucz obcy do filmu
-    public Movie? Movie { get; set; } // NAZWA właściwości musi być TAKA SAMA jak powyżej + "Movie"
+    public int MovieId { get; set; } 
+    public Movie? Movie { get; set; } 
 
     public DateTime ScreeningTime { get; set; }
-    // Screening.cs
+    
     public string HallNumber { get; set; } = string.Empty;
 
 }
